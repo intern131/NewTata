442d55de21396f719c5411860051a7f9c83a6f2e
intern131 <intern.mobicloud@gmail.com>
2024-09-11 12:05:39 +0530

    changes

--- src/app/dashboard/consumer-dashboard/consumer-dashboard.component.ts
+++ src/app/dashboard/consumer-dashboard/consumer-dashboard.component.ts
@@ -1,6 +1,6 @@
 import { RouterLink } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { Component, HostListener } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-consumer-dashboard',
@@ -13,13 +13,25 @@ export class ConsumerDashboardComponent {
   isProfileMenuOpen = false;
   isMenuOpen = false;
 
+  constructor(private location: Location) {}
+
   // Toggle profile dropdown menu
   toggleProfileMenu() {
     this.isProfileMenuOpen = !this.isProfileMenuOpen;
   }
 
+  // Close profile menu
+  closeProfileMenu() {
+    this.isProfileMenuOpen = false;
+  }
+
   // Toggle sidebar menu
   toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  // Back button functionality for sidebar menu
+  goBack() {
     this.isMenuOpen = !this.isMenuOpen;
   }
 }
