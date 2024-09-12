2f5d989b6c3f4375f686518c61e69007b0da11e2
intern131 <intern.mobicloud@gmail.com>
2024-09-12 12:42:19 +0530

    feedback page

--- src/app/pages/consumer/feedback/feedback.component.ts
+++ src/app/pages/consumer/feedback/feedback.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormBuilder } from '@angular/forms';
+import { NavbarComponent } from "../../../common/navbar/navbar.component";
 
 @Component({
   selector: 'app-feedback',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, CommonModule, ReactiveFormsModule],
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.scss'
 })
 export class FeedbackComponent {
+  feedbackForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.feedbackForm = this.fb.group({
+      category: ['General'],
+      message: [''],
+      file: [null],
+    });
+  }
+
+  onSubmit() {
+    if (this.feedbackForm.valid) {
+      console.log('Feedback submitted:', this.feedbackForm.value);
+    }
+  }
 }
