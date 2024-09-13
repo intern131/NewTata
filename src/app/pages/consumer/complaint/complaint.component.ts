4d35436d624401713183ef6570da08677aa52bbc
intern131 <intern.mobicloud@gmail.com>
2024-09-13 15:40:54 +0530

    complaint form done

--- src/app/pages/consumer/complaint/complaint.component.ts
+++ src/app/pages/consumer/complaint/complaint.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NavbarComponent } from "../../../common/navbar/navbar.component";
 
 @Component({
   selector: 'app-complaint',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, NavbarComponent],
   templateUrl: './complaint.component.html',
   styleUrl: './complaint.component.scss'
 })
-export class ComplaintComponent {
+export class ComplaintComponent
+{
+  complaintForm: FormGroup;
+  complaintHistory = [
+    { date: '10 Sep 2024', subject: 'Battery Issue', status: 'Pending' },
+    { date: '9 Aug 2024', subject: 'Connection Issue', status: 'Completed' },
+  ];
 
+  constructor(private fb: FormBuilder) {
+    this.complaintForm = this.fb.group({
+      subject: ['', Validators.required],
+      description: ['', Validators.required],
+      attachments: ['']
+    });
+  }
+
+  onSubmitComplaint() {
+    if (this.complaintForm.valid) {
+      const newComplaint = {
+        date: new Date().toLocaleDateString(),
+        subject: this.complaintForm.value.subject,
+        status: 'Pending'
+      };
+      this.complaintHistory.unshift(newComplaint);
+      this.complaintForm.reset();
+    }
+  }
 }
