792ff9bd6c444201edf84a53ac60c73c4bc99e52
intern131 <intern.mobicloud@gmail.com>
2024-09-16 11:31:00 +0530

    aleart page done

--- src/app/pages/consumer/alert/alert.component.ts
+++ src/app/pages/consumer/alert/alert.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../../common/navbar/navbar.component";
 
 @Component({
   selector: 'app-alert',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './alert.component.html',
   styleUrl: './alert.component.scss'
 })
 export class AlertComponent {
+  alerts = [
+    { message: 'Low Battery: Meter #1234', time: '2 hours ago' },
+    { message: 'High Gas Consumption: Meter #1234', time: '1 day ago' },
+    { message: 'Communication Fault: Meter #1234', time: '2 days ago' }
+  ];
 
+  description = "Battery level dropped below 20%. Please schedule maintenance.";
 }
